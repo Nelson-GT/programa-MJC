af9b086616bfdc0b0caa9dc3b8422b87ffe87156
luisyepez11 <luisrobert11660@gmail.com>
2025-07-07 23:43:33 -0400

    descarga de planillas

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -9,6 +9,7 @@ const Button = ({ children, className = '', ...props }: ButtonProps) => (
         className={`${className} px-4 py-2.5 font-medium text-sm text-center duration-150`}
     >
         {children}
+        
     </button>
 );
 
